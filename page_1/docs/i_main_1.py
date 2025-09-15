7157adf7436e96d87312fd837aeeeb1d9ede8052
IPrincipalCentral2 <deboubil18@gmail.com>
2025-09-15 22:01:41 +0100

    i_hello

---
+++ page_1/docs/i_main_1.py
@@ -0,0 +1,161 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+list_of_liberary_to_install = [
+
+                            ["PyQt5"] ,
+                            
+                            
+
+
+
+]
+
+
+
+
+
+
+
+
+
+
+import os
+
+
+import traceback
+
+import sys
+
+
+import subprocess
+
+
+
+
+
+try:
+    
+    counter_0 = 0
+    
+    
+    while (counter_0 < len(list_of_liberary_to_install)):
+    
+
+                
+        try:
+        
+        
+            print(f"\n\n\npip install {list_of_liberary_to_install[counter_0][0]}\n\n\n")
+            
+            subprocess.check_call([sys.executable, "-m", "pip", "install", f"{list_of_liberary_to_install[counter_0][0]}"])
+        
+        
+                
+        except:
+        
+                
+                        
+            traceback.print_exc()
+            
+            error = traceback.format_exc()
+            
+            semaphore = True
+            
+            print(f"Erreur : {str(error)}")
+            
+        
+        
+        counter_0 += 1
+        
+        
+    
+except:
+
+        
+                
+    traceback.print_exc()
+    
+    error = traceback.format_exc()
+    
+    semaphore = True
+    
+    print(f"Erreur : {str(error)}")
+    
+    
+    
+
+
+print("\n" * 10)
+
+
+
+
+
+import sys
+import webbrowser
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("فتح رابط بالمتصفح")
+        self.setGeometry(200, 200, 300, 150)
+
+        layout = QVBoxLayout()
+
+        # Label
+        self.label = QLabel("اضغط على الزر لفتح الرابط:", self)
+        layout.addWidget(self.label)
+
+        # Button
+        self.button = QPushButton("فتح الرابط", self)
+        self.button.clicked.connect(self.open_link)
+        layout.addWidget(self.button)
+
+        self.setLayout(layout)
+
+    def open_link(self):
+        
+                
+        # ضع الرابط الذي تريده هنا
+        
+        
+        url = "https://iprincipalcentral2.github.io/website1/page_1/page_of_work_0.html"
+        
+        webbrowser.open(url)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
