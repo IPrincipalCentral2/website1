55889ccf9cec1159134b0ec847ceec2ad8d445fc
IPrincipalCentral2 <deboubil18@gmail.com>
2025-09-27 14:13:17 +0100

    i_hello

---
+++ space_0/i_main_0_i.py
@@ -0,0 +1,181 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+i_list_of_name_male_0_i = [
+                    
+                    
+                    
+                    "Adrian",
+                    
+                    "Leonardo",
+                    
+                    "Damian",
+                    
+                    "Alexander",
+                    
+                    "Mateo",
+                    
+                    "Julian",
+                    
+                    "Lorenzo",
+                    
+                    "Enzo",
+                    
+                    "Ethan",
+                    
+                    "Sebastian",
+                    
+                    ]
+
+
+
+
+i_list_of_name_female_0_i = [
+                    
+                    
+                    
+                    
+                    
+                    "Isabella",
+                    
+                    "Sophia",
+                    
+                    "Valentina",
+                    
+                    "Layla",
+                    
+                    "Selena",
+                    
+                    "Ariana",
+                    
+                    "Camila",
+                    
+                    "Scarlett",
+                    
+                    "Adriana",
+                    
+                    "Jasmine",
+                    
+                    
+                    
+                    
+                    ]
+
+
+
+
+
+i_c_0_i = "{"
+
+
+i_c_1_i = "}"
+
+
+i_list_0_i = i_list_of_name_male_0_i
+
+
+
+
+i_counter_0_i = 0
+
+while (i_counter_0_i < len(i_list_0_i) - 1):
+    
+    
+    print(f"{i_c_0_i}\n\n")
+    
+    
+    print(f"    \"name\" : \"{i_list_0_i[i_counter_0_i]}\" , \n\n")
+    
+    
+    print(f"    \"gender\" : \"male\" ,\n\n")
+    
+    
+    print(f"    \"per_Day\" : {len(i_list_0_i) - i_counter_0_i + 1} \n\n")
+    
+    
+    print(f"{i_c_1_i} , \n\n")
+    
+    
+    i_counter_0_i += 1
+    
+
+
+
+print(f"{i_c_0_i}\n\n")
+
+
+print(f"    \"name\" : \"{i_list_0_i[i_counter_0_i]}\" , \n\n")
+
+
+print(f"    \"gender\" : \"male\" , \n\n")
+
+
+print(f"    \"per_Day\" : {len(i_list_0_i) - i_counter_0_i + 1} \n\n")
+
+
+print(f"{i_c_1_i} , \n\n")
+
+
+
+
+
+
+i_list_0_i = i_list_of_name_female_0_i
+
+
+i_counter_0_i = 0
+
+while (i_counter_0_i < len(i_list_0_i) - 1):
+    
+    
+    print(f"{i_c_0_i}\n\n")
+    
+    
+    print(f"    \"name\" : \"{i_list_0_i[i_counter_0_i]}\" , \n\n")
+    
+    
+    print(f"    \"gender\" : \"female\" , \n\n")
+    
+    
+    print(f"    \"per_Day\" : {len(i_list_0_i) - i_counter_0_i + 1} \n\n")
+    
+    
+    print(f"{i_c_1_i} , \n\n")
+    
+    
+    i_counter_0_i += 1
+    
+
+
+
+print(f"{i_c_0_i}\n\n")
+
+
+print(f"    \"name\" : \"{i_list_0_i[i_counter_0_i]}\" , \n\n")
+
+
+print(f"    \"gender\" : \"female\" , \n\n")
+
+
+print(f"    \"per_Day\" : {len(i_list_0_i) - i_counter_0_i + 1} \n\n")
+
+
+print(f"{i_c_1_i} \n\n")
+
+
+
+
+
+
+
